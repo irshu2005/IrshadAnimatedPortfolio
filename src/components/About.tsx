0f2778f812323fb7acc8be109eb7c6d310f11a7c
Irshad Ahamad <144877743+irshu2005@users.noreply.github.com>
2026-08-04 11:31:01 +0530

    Revise About Me section with new content
    Updated personal introduction and professional experience details.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,8 +6,9 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          I am a Computer Science student with hands-on experience in full-stack development and real-world project implementation. Currently working as a Software Tools and Platform Engineering Intern at SmartFRS pvt ltd, where I am involved in building and testing scalable applications. I have a strong foundation in Java, SpringBoot, and REST APIs, and I am passionate about solving real-world problems while continuously improving my technical and professional skills.
-        </p>
+Hi, I'm Irshad Ahamad, a Computer Science graduate and Backend Developer passionate about building scalable and high-performance applications.
+
+I have 6+ months of professional experience working with Java, Spring Boot, Spring WebFlux, REST APIs, MongoDB, and Redis. I started my professional journey as a Backend Developer at SmartFRS Solutions Pvt. Ltd., where I developed backend services, designed RESTful APIs, collaborated with frontend and QA teams, and contributed to technical documentation. Currently, I’m working as a Software Trainee at Bizlyzer India Pvt. Ltd. (OmniXM), where I continue to enhance my backend development skills while contributing to enterprise-grade software solutions. I'm always excited to take on new challenges, collaborate with talented teams, and build software that creates meaningful impact.        </p>
       </div>
     </div>
   );
